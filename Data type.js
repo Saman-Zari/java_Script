4162a7f1057ef017fa5f3850d1b5daca459091bf
samanzari7 <samanzari71@gmail.com>
2024-02-05 18:16:30 +0500

    datatype

---
+++ Data type.js
@@ -0,0 +1,18 @@
+//alert(3+3) //we are using nodejs , not browser
+console.log(3+3);
+
+console.log("Saman zari");
+// code readability should be high
+let name = "Samn zari " //string
+let age = 22 //number
+// BigInt
+//Boolean
+//Null
+//Undefine
+//Symbol
+
+//Object
+// null is a object
+
+console.log(typeof undefined); //this output is undefine
+console.log(typeof null);  //this type is object
