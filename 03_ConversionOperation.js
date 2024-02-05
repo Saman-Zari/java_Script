92f40ceddd1992b206fbf14d7e41308ec6a009bb
samanzari7 <samanzari71@gmail.com>
2024-02-05 18:49:47 +0500

    ConverionOperation

---
+++ 03_ConversionOperation.js
@@ -0,0 +1,40 @@
+let score = "sam "
+
+console.log(typeof score);
+console.log(typeof (score));
+let valueInNumer = Number(score)
+console.log(typeof valueInNumer);
+console.log(valueInNumer);
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+// Number coversion into boolean 
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+//1 => true; 0=> fase
+// "" => false
+// "Name" => true
+
+// number convert to string
+ 
+let number = 55
+let stringnumber = String(number)
+
+console.log( typeof stringnumber);
+
+let a = 55
+let b = "sam"
+let c = Number(b)
+let d = a + b
+console.log(typeof a);
+console.log(typeof c);
+console.log(d);
+let e = Number(d)
+console.log(typeof e);
