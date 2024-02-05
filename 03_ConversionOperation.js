fa75276ee8f15c99001ed12dabd79bbf943600ea
samanzari7 <samanzari71@gmail.com>
2024-02-05 19:14:09 +0500

    OPERATION

--- 03_ConversionOperation.js
+++ 03_ConversionOperation.js
@@ -1,10 +1,10 @@
 let score = "sam "
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 let valueInNumer = Number(score)
-console.log(typeof valueInNumer);
-console.log(valueInNumer);
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);
 
 //"33" => 33
 //"33abc" => NaN
@@ -15,8 +15,8 @@ console.log(valueInNumer);
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 //1 => true; 0=> fase
 // "" => false
@@ -27,14 +27,44 @@ console.log(typeof booleanIsLoggedIn);
 let number = 55
 let stringnumber = String(number)
 
-console.log( typeof stringnumber);
+// console.log( typeof stringnumber);
 
 let a = 55
 let b = "sam"
 let c = Number(b)
 let d = a + b
-console.log(typeof a);
-console.log(typeof c);
-console.log(d);
-let e = Number(d)
-console.log(typeof e);
+// console.log(typeof a);
+// console.log(typeof c);
+// console.log(d);
+// let e = Number(d)
+// console.log(typeof e);
+// ************* Operation ************
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "sam"
+let str2 = " zari"
+
+let str3 =str1 + str2
+console.log(str3);
+
+// console.log("2" + 1);
+// console.log(1 + "2");
+// console.log("10" + 2 + 6);
+// console.log((2 + 5 + "5"));
+
+// console.log(+true);
+// console.log(+ " ");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+console.log(num1);
+
+let gameConter = 100
+++gameConter;
+console.log(gameConter);
