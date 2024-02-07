1231df51422c363fb28f00759c318e8e58b350e3
samanzari7 <samanzari71@gmail.com>
2024-02-07 11:05:01 +0500

    stack

--- comaparision.js
+++ comaparision.js
@@ -2,3 +2,7 @@ console.log("2" === 2);
 console.log(null > 0);
 console.log(null == 0);
 console.log(null >= 0);
+
+// Stack ( Primitive),when  stack memory is used its mean you decare any variable it return its copy 
+// Heap(Non- Primitive) when memeory define in heap you found a refrence of  value 
+
